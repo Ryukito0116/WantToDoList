cd0e1dcdb363d9bd765e532cb98b9331b40de96e
Ryukito0116 <Ryukito.rabbit.0116@icloud.com>
2019-12-04 10:36:14 +0900

    12月4日更新やで

--- index.php
+++ index.php
@@ -14,19 +14,25 @@
 </header>
 <div class="userinfo">
     <div class="infoBoxTop">
-        <h1>あなたのやりたいことリスト</h1>
+        <h2>あなたのやりたいことリスト</h2>
     </div>
     <div class="infoBoxBottom">
-        <div class="left33">
+        <div class="left">
             <div class="fhoto_circle"></div>
         </div>
-        <div class="right66">
-            <h3>やりたいことの数</h3>
-            <h3>主な活動拠点</h3>
-            <h3>暇な時間帯</h3>
-            <h3>職業</h3>
+        <div class="center">
+            <h3 class="info">やりたいことの数</h3>
+            <h3 class="info">主な活動拠点</h3>
+            <h3 class="info">フォロワー</h3>
+            <h3 class="info">達成率</h3>
             <button onclick="location.href='formpage.html'">追加</button>
             <button>達成済み</button>
+        </div>
+        <div class="right">
+            <h3>  :12</h3>
+            <h3>  :神戸</h3>
+            <h3>  :256</h3>
+            <h3>  :100%</h3>
         </div>
     </div>
 </div>
@@ -37,7 +43,33 @@
 <br>
 <div class="WantToDoList">
     <?php
-        echo $_POST["WhatDo"];
+        // データベースに接続
+        $db = new mysqli("localhost","root","Gosto0116","project_goodlife");
+        if ($db->connect_error) {
+            echo $db->connect_error;
+            exit();
+        } /*else {
+            echo "sucsess!";
+            $db->set_charset("utf8");
+        }*/
+
+        echo "<table>";
+        //SQL分でデータを取得
+        $sql = "SELECT * from goodlist";
+        if ($result = $db->query($sql)) {
+            //連想配列を取得
+            while ($row = $result->fetch_assoc()) {
+                echo "<div class='DBlist'>
+                        <tr class='DBlis'><td>" . $row["id"] . "</td><td>" . $row["name"] . "</td><td>" . $row["wantdoing"] . "</td><td>" . $row["category"] . "</td><td>" . $row["withdo"] . "</td><td>" . $row["fin"] . "</td><td>" . $row["good"] . "</td><td>" . $row["metoo"] . "</td></tr>
+                      </div>";
+            }
+            //結果を閉じる
+            $result->close();
+        }
+        echo "</table>";
+
+        //データベース切断
+        $db->close();
     ?>
 </div>
 </body>
